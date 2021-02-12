3f258fe9789a19891ce806e668b2b5e5bd6ea103
Kaan Demirtaş <kaandemirtas1@outlook.com>
2021-02-12 12:18:33 +0300

    30.01.2021

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -46,6 +46,11 @@ namespace DataAccess.Concrete.InMemory
             return _products;
         }
 
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
         public void Update(Product product)
         {   //Gönderdiğim ürün id'sini sahip olan listedeki ürünü bul
             Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
